45bc804f1f578cfe16684b4dcef34811460aa4a6
dom <zero.trust.brain@gmail.com>
2024-02-12 02:09:26 +0000

    add database utilies with DatabaseCOnnector class and method to read databasse creds from yaml file

--- database_utils.py
+++ database_utils.py
@@ -1,6 +1,6 @@
+import yaml
 #Create another script named database_utils.py and within it, create a class DatabaseConnector which you will use to connect with and upload data to the database.
 class DatabaseConnector:
-    #DatabaseConnector will be to connect with and upload data to the database.
-    def __init__(self, database_config):
-        def upload_data(self, data, table_name):
-            pass
+    def read_db_creds(self):
+        with open("db_creds.yaml", "r") as file:
+            return yaml.safe_load(file)
--- read_db_creds.py
+++ read_db_creds.py
@@ -4,3 +4,4 @@ def read_db_creds():
     with open('db_creds.yaml', 'r') as file:
         credentials = yaml.safe_load(file)
     return credentials
+    print(credentials)
