113b9b7d18aeb5b0ec0ad2e134e4d33945450b79
dom <zero.trust.brain@gmail.com>
2024-02-21 10:45:46 +0000

    final changes before Data Sage

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,4 +1,21 @@
+#%%
+import pandas as pd
 class DataCleaning:
+    # method for cleaning data extracted from CSV file
     def clean_csv_data(self, data):
-        # Placeholder method for cleaning data extracted from a CSV file
-        pass    
+        pass
+    
+    # clean_card_data method in the DataCleaning class (takes )
+    def clean_card_data(self, df):
+        try:
+            # Remove any rows with NULL values
+            df_cleaned = df.dropna()
+            # Remove erroneous values or errors with formatting (customize as needed)
+            # Example: Remove rows where the 'Amount' column is negative
+            df_cleaned = df_cleaned[df_cleaned['Amount'] >= 0]
+            return df_cleaned
+        except Exception as e:
+            print("Error cleaning data:", str(e))
+            return None    
+    
+
--- database_utils.py
+++ database_utils.py
@@ -1,6 +1,32 @@
 import yaml
+
+import psycopg2
+
+from sqlalchemy import create_engine
+from sqlalchemy import inspect
+from sqlalchemy import text
+import pandas as pd
+
+# use Properties section in pgAdmin to get the details
+DATABASE_TYPE = 'postgresql'
+DBAPI = 'psycopg2'
+HOST = 'localhost'
+USER = 'postgres'
+PASSWORD = 'Jenkins09'
+DATABASE = 'sales_data'
+PORT = 5436
 #Create another script named database_utils.py and within it, create a class DatabaseConnector which you will use to connect with and upload data to the database.
 class DatabaseConnector:
     def read_db_creds(self):
         with open("db_creds.yaml", "r") as file:
-            return yaml.safe_load(file)
+            creds = yaml.safe_load(file)    
+        return creds
+
+def init_db_engine(self, credentials):
+    try:
+        # Create a connection to the database
+        engine = create_engine("mysql+pymysql://" + credentials['user'] + ":" + credentials['password'] + "@" + credentials['host'] + ":" + credentials['port'] + "/" + credentials['database'])
+        return engine
+    except Exception as e:
+        print("Error connecting to the database:", str(e))
+        return None
